0f2c2283a3561ed12bbe1620f7f4cec38bf85e28
uabdirasulm <ubbiniyazov04@gmail.com>
2024-04-18 18:54:30 +0500

    add popular products section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,27 @@
+import { products } from "../constants";
+import PopularProductCard from "../components/PopularProductCard";
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="#products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+          Our collection is curated to provide you with the best quality
+          products for your active lifestyle. From the latest arrivals to the
+          most popular items, we have something for everyone. Explore our
+          collection and find the perfect pairing for your workout.
+        </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
