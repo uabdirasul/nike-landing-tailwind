cf87cf92d339bda15cf572c8399ef7bed6397de4
uabdirasulm <ubbiniyazov04@gmail.com>
2024-04-28 06:28:55 +0500

    added customer reviews section

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,31 @@
+import { star } from "../assets/icons";
+
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <img
+        src={imgURL}
+        alt="customer image"
+        width={120}
+        height={120}
+        className="rounded-full"
+      />
+      <p className="m-6 max-w-sm text-center info-text">{feedback}</p>
+      <div className="mt-3 flex justify-center items-center gap-2.5">
+        <img
+          src={star}
+          alt="rating image"
+          width={24}
+          height={24}
+          className="object-contain m-0"
+        />
+        <p className="text-xl font-montserrat text-slate-gray">{rating}</p>
+      </div>
+      <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">
+        {customerName}
+      </h3>
+    </div>
+  );
+};
+
+export default ReviewCard;
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,28 @@
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
+
 const CustomerReviews = () => {
-  return <div>CustomerReviews</div>;
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-4xl text-center font-bold">
+        What our <span className="text-coral-red">Customers</span> say
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine feedback from our customers.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustomerReviews;
